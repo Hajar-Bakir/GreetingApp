6b3ca2a52e8ac5ad3eee735614b978a0c78f7718
hajarbakir <hajar.bakir@docc.techstarter.de>
2024-02-29 12:27:55 +0100

    update

--- greeting.test.js
+++ greeting.test.js
@@ -1,6 +1,7 @@
-// greeting.test.js
-const greeting = require('./greeting');
+const { calculateBMI } = require('./GREETING-APP/main.js');
 
-test('Greeting returns correct message', () => {
-  expect(greeting()).toBe('Hello, World!');
+test('Hello World', () => {
+  expect(calculateBMI(70, 175)).toBe(23);
+  expect(calculateBMI(85, 190)).toBe(24);
+  expect(calculateBMI(65, 160)).toBe(25);
 });
