53a7b4ba0bac2669fdd3c3e7b0a979c3a548518f
hajarbakir <hajar.bakir@docc.techstarter.de>
2024-02-29 11:33:46 +0100

    update

---
+++ server.js
@@ -0,0 +1,16 @@
+// Laden des Express-Moduls
+const express = require("express");
+
+// Portnummer für den Server wird hier festgelegt!
+const port = process.env.PORT || 3000;
+
+// Express-Anwendung wird erstellt und in die Variable "app" importiert.
+let app = express();
+
+// Middleware, um statische Dateien aus dem "frontend" Ordner bereitzustellen
+app.use(express.static('frontend'));
+
+// Server starten und auf Verbindungen auf dem angegebenen Port warten
+app.listen(port, () => {
+  console.log(`Server läuft auf Port ${port}`);
+});
